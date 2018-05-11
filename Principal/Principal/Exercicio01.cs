e82579b38a7240c6b7b566db62ed62b952d0f363
Marcos Luiz Mittag Filho <Marquinhoszx_xd@Outlook.com>
2018-05-11 16:14:18 -0300

    Exercicio01

---
+++ Principal/Principal/Exercicio01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Principal
+{
+    public partial class Exercicio01 : Form
+    {
+        public Exercicio01()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(tbnumero.Text);
+            int sucessor = numero + 1;
+            int antecessor = numero - 1;
+
+            MessageBox.Show("sucessor: " + sucessor + "\n"+
+                            "Antecessor: " + antecessor);
+            
+        }
+    }
+}
